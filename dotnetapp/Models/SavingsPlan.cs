427dcf38b3a7b3eb3760aa8d423f04728f5f54a0
neojarvis <techadmin@iamneo.ai>
2025-05-27 12:59:23 +0000

    Manual-save-commit-5/27/2025--6:29:21-PM

---
+++ dotnetapp/Models/SavingsPlan.cs
@@ -0,0 +1,7 @@
+public int SavingsPlanId {get; set}
+public string Name {get; set}
+public decimal GoalAmount {get; set}
+public int TimeFrame {get; set}
+public string RiskLevel {get; set}
+public string Description {get; set}
+public string Status {get; set}
