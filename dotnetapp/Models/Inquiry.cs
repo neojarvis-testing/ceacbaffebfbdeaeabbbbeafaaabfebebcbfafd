d92e67e720b1f7f2b12d510d0953c0a4ac41df86
neojarvis <techadmin@iamneo.ai>
2025-05-27 12:57:26 +0000

    Manual-save-commit-5/27/2025--6:27:23-PM

---
+++ dotnetapp/Models/Inquiry.cs
@@ -0,0 +1,6 @@
+public int UserId {get; set;}
+public string Email  {get; set;}
+public string Password {get; set;}
+public string UserName {get; set;}
+public string MobileNumber {get; set;}
+public string UserRole {get; set;}
