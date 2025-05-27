7dce6c8d4ce467aeb7376d5fae1fa446ac9798bd
neojarvis <techadmin@iamneo.ai>
2025-05-27 13:04:45 +0000

    Manual-save-commit-5/27/2025--6:32:26-PM

---
+++ dotnetapp/Models/PlanApplication.cs
@@ -0,0 +1,10 @@
+public int PlanApplicationId {get; set;}
+public int UserId {get; set;}
+public SavingsPlanId {get; set;}
+public decimal AppliedAmount {get; set;}
+public string Status {get; set;}
+public DateTime ApplicationDate {get; set;}
+public string? Remarks {get; set;}
+public string? ProofDocument {get; set;}
+public User? User {get; set;}
+public SavingsPlan? SavingsPlan {get; set;}
