931ee4bb47e48e544c286b85f5c2e1c583279d88
neojarvis <techadmin@iamneo.ai>
2025-05-29 14:53:14 +0000

    Manual-save-commit-5/29/2025--8:23:10-PM

--- reactapp/src/App.test.js
+++ reactapp/src/App.test.js
@@ -7,3 +7,12 @@ test('renders login page',() =>
     render(<App/>);
     expect(screen.getByText(/Login/i)).toBeInTheDocument();
 });
+
+test('allows login with correct credentials', () =>
+{
+    render(<App/>);
+    fireEvent.click(screen.getByText(SignUp/i));
+    fireEvent.change(screen.getByPlaceHolderText(/Name/i), {target: {value: 'Test User'}});
+    
+}
+)
