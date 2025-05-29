048764b0d2a5437f81946cee744153745287f85d
neojarvis <techadmin@iamneo.ai>
2025-05-29 14:44:34 +0000

    Manual-save-commit-5/29/2025--8:14:29-PM

--- reactapp/src/Components/Signup.jsx
+++ reactapp/src/Components/Signup.jsx
@@ -1,4 +1,12 @@
 function SignUp()
 {
-    const [form, setForm] = useState({name:'', email:'', password:'', role:role.CLIENT});
+    const [form, setForm] = useState({name:'', email:'', password:'', role:regUserRoles.CLIENT});
+    const [registered, setRegistered] = userState(false);
+    const handleChange = e => setForm({...form,[e.target.name]: e.target.value});
+    const handleRegister = () => 
+    {
+        regUserDB.push(form);
+        setRegistered(true);
+    };
+    
 }
